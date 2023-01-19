53299bb61bef0d807b3002d7375784acf2ca81e9
Ben <ben@rocketmakers.com>
2023-01-19 10:39:54 +0000

    Remove global resize

--- src/utils/global_resize.js
+++ src/utils/global_resize.js
@@ -21,12 +21,12 @@ import { EventMultiplexer } from '../utils/event_multiplexer.js';
 import { subscribeDOMEvent } from '../utils/events.js';
 
 export const GlobalResize = new EventMultiplexer((cb) => {
-  const sub1 = subscribeDOMEvent(window, 'resize', () => cb());
+  // const sub1 = subscribeDOMEvent(window, 'resize', () => cb());
   const sub2 = subscribeDOMEvent(window, 'load', () => cb());
   const sub3 = subscribeDOMEvent(document.fonts, 'loadingdone', () => cb());
 
   return () => {
-    sub1();
+    // sub1();
     sub2();
     sub3();
   };
