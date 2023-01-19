fb159c357eb43fe66748f51eba22cb650736f926
Ben <ben@rocketmakers.com>
2023-01-19 10:51:22 +0000

    Remove all resize event triggers

--- src/utils/global_resize.js
+++ src/utils/global_resize.js
@@ -22,12 +22,12 @@ import { subscribeDOMEvent } from '../utils/events.js';
 
 export const GlobalResize = new EventMultiplexer((cb) => {
   // const sub1 = subscribeDOMEvent(window, 'resize', () => cb());
-  const sub2 = subscribeDOMEvent(window, 'load', () => cb());
-  const sub3 = subscribeDOMEvent(document.fonts, 'loadingdone', () => cb());
+  // const sub2 = subscribeDOMEvent(window, 'load', () => cb());
+  // const sub3 = subscribeDOMEvent(document.fonts, 'loadingdone', () => cb());
 
   return () => {
     // sub1();
-    sub2();
-    sub3();
+    // sub2();
+    // sub3();
   };
 });
